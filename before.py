66f83f3d3a9e8e98b55ec8a0cb33b8a535d10fca
minyoungjun <minyoung9353@gmail.com>
2017-12-22 14:09:37 +0000

    setting

--- before.py
+++ before.py
@@ -1,16 +1,17 @@
 import csv
 import uuid
+import datetime
 
-ff = open('after1.csv', 'w')
+ff = open('after3.csv', 'w')
 wr = csv.writer(ff)
-with open('after.csv', 'r') as f:
+with open('bitstampUSD_1-min_data_2012-01-01_to_2017-10-20.csv', 'r') as f:
 
     rdr = csv.reader(f)
     next(rdr, None)
-    counter = 0
-    iid = uuid.uuid4()
+    counter = 1
     for line in rdr:
-        line.insert(0, iid)
+        line[0] = datetime.datetime.fromtimestamp(int(line[0])).strftime('%Y-%m-%d %H:%M:%S')
+        line.insert(0, counter)
         wr.writerow(line)
         counter = counter + 1
         print(counter)
